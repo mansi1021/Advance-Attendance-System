ef7f438333b025c95f7c12de606e2359917fe691
Mansi Kanwar <85470360+mansi1021@users.noreply.github.com>
2023-05-29 09:32:06 +0530

    train.py

--- train.py
+++ train.py
@@ -22,24 +22,24 @@ class Train:
         # img_top = img_top.resize((1920, 325), Image.ANTIALIAS)
         # self.photoimg_top = ImageTk.PhotoImage(img_top)
 
-        img = Image.open(r"college_images\img1.jpeg")
-        img = img.resize((480,250),Image.ANTIALIAS)
+        img = Image.open(r"college_images\img1.jpeg") # type: ignore
+        img = img.resize((480,250),Image.ANTIALIAS) # type: ignore
         self.photoimg = ImageTk.PhotoImage(img)
 
         f_lbl = Label(self.root,image = self.photoimg)
         f_lbl.place(x=0,y=0,width=480,height=250)
 
         # second image
-        img1 = Image.open(r"college_images\img3.jpeg")
-        img1 = img1.resize((480,250),Image.ANTIALIAS)
+        img1 = Image.open(r"college_images\img3.jpeg") # type: ignore
+        img1 = img1.resize((480,250),Image.ANTIALIAS) # type: ignore
         self.photoimg1 = ImageTk.PhotoImage(img1)
 
         f_lbl = Label(self.root,image = self.photoimg1)
         f_lbl.place(x=480,y=0,width=480,height=250)
 
         # third image
-        img2 = Image.open(r"college_images\img4.jpeg")
-        img2 = img2.resize((480,250),Image.ANTIALIAS)
+        img2 = Image.open(r"college_images\img4.jpeg") # type: ignore
+        img2 = img2.resize((480,250),Image.ANTIALIAS) # type: ignore
         self.photoimg2 = ImageTk.PhotoImage(img2)
 
         f_lbl = Label(self.root,image = self.photoimg2)
@@ -47,8 +47,8 @@ class Train:
 
 
         # fourth image
-        img3 = Image.open(r"college_images\img5.jpeg")
-        img3 = img3.resize((480,250),Image.ANTIALIAS)
+        img3 = Image.open(r"college_images\img5.jpeg") # type: ignore
+        img3 = img3.resize((480,250),Image.ANTIALIAS) # type: ignore
         self.photoimg3 = ImageTk.PhotoImage(img3)
 
         f_lbl = Label(self.root,image = self.photoimg3)
@@ -57,10 +57,10 @@ class Train:
         # button
         b1_1 = Button(self.root, text="TRAIN DATA", command=self.train_classifier, cursor="hand2",
                       font=("times new roman", 30, "bold"), bg="red", fg="white")
-        b1_1.place(x=0, y=380, width=1920, height=60)
+        b1_1.place(x=0, y=380, width=1550, height=70)
 
-        img_bottom = Image.open(r"college_images\img25.jpeg")
-        img_bottom = img_bottom.resize((1920, 425), Image.ANTIALIAS)
+        img_bottom = Image.open(r"college_images\img25.jpeg") # type: ignore
+        img_bottom = img_bottom.resize((1920, 425), Image.ANTIALIAS) # type: ignore
         self.photoimg_bottom = ImageTk.PhotoImage(img_bottom)
 
         f_lbl = Label(self.root, image=self.photoimg_bottom)
@@ -74,7 +74,7 @@ class Train:
         ids = []
 
         for image in path:
-            img = Image.open(image).convert('L')  # Gray Scale image
+            img = Image.open(image).convert('L')  # type: ignore
             imageNp = np.array(img, 'uint8')
             id = int(os.path.split(image)[1].split('.')[1])
 
